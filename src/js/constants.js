c95b24d12f69677bf0f20010446b3107eb69f4df
Євгенія Швець <evheniia@MacBook-Air-Evgenia.local>
2025-08-17 12:26:43 +0300

    products by category almost done

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,6 +3,7 @@ export const BASE_URL = 'https://dummyjson.com';
 export const ENDPOINTS = {
   CATEGORIES: '/products/category-list',
   PRODUCTS: '/products',
+  CATEGORY: '/products/category/',
 };
 
 export const PAGE_SIZE = 12;
